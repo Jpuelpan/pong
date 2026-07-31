f6923c14196d59adec9a35aee8e0fcddbb45f7ce
Juan Puelpan <juan@puelpan.com>
2026-07-31 00:49:33 -0400

    moving players and bouncing ball

--- main.c
+++ main.c
@@ -14,38 +14,78 @@ SDL_FColor FG_COLOR = {0xFF, 0xFF, 0xFF, 0xFF};
 SDL_FColor BALL_COLOR = {0x00, 0x00, 0xFF, 0xFF};
 
 typedef struct {
-  float x;
-  float y;
+  SDL_FRect rect;
   float dx;
   float dy;
   float speed;
 } Ball;
 
 typedef struct {
-  float x;
-  float y;
+  SDL_FRect rect;
   float dy;
   float speed;
-} Paddle;
+  int score;
+} Player;
 
 typedef struct {
-  Ball ball;
-  Paddle left_paddle;
-  Paddle right_paddle;
+  bool is_paused;
+  bool next_step;
 
+  Ball ball;
+  Player left_player;
+  Player right_player;
   Uint64 last_ticks;
 } GameState;
+
+void initialize_ball(Ball *ball) {
+  ball->rect.x = WIN_WIDTH / 2.0;
+  ball->rect.y = WIN_HEIGHT / 2.0;
+  ball->rect.w = 20;
+  ball->dx = -1.0;
+  ball->dy = -1.0;
+  ball->speed = START_SPEED;
+}
+
+void initialize_game(GameState *game) {
+  initialize_ball(&game->ball);
+  game->is_paused = false;
+
+  game->left_player.rect.h = 100;
+  game->left_player.rect.w = 20;
+  game->left_player.rect.x = 5;
+  game->left_player.rect.y = WIN_HEIGHT / 2.0 - game->left_player.rect.h / 2.0;
+  game->left_player.speed = START_SPEED;
+  game->left_player.dy = 0.0;
+  game->left_player.score = 0;
+
+  game->right_player.rect.h = 100;
+  game->right_player.rect.w = 20;
+  game->right_player.rect.x = WIN_WIDTH - game->left_player.rect.w - 5;
+  game->right_player.rect.y = WIN_HEIGHT / 2.0 - game->left_player.rect.h / 2.0;
+  game->right_player.speed = START_SPEED;
+  game->right_player.dy = 0.0;
+  game->right_player.score = 0;
+
+  game->last_ticks = SDL_GetTicks();
+}
+
+void RenderBall(float x, float y, float size) {
+  SDL_FRect r = {x - size / 2.0, y - size / 2.0, size, size};
+  SDL_RenderRect(renderer, &r);
+  SDL_RenderFillRect(renderer, &r);
+}
+
+void RenderPlayer(Player *player) {
+  SDL_FRect r = {player->rect.x, player->rect.y, player->rect.w,
+                 player->rect.h};
+  SDL_RenderRect(renderer, &r);
+  SDL_RenderFillRect(renderer, &r);
+}
 
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[]) {
   SDL_Log("Starting pong...");
   GameState *game = SDL_calloc(1, sizeof(GameState));
-
-  game->ball.x = WIN_WIDTH / 2.0;
-  game->ball.y = WIN_HEIGHT / 2.0;
-  game->ball.dx = 1.0;
-  game->ball.dy = -1.0;
-  game->ball.speed = START_SPEED;
-  game->last_ticks = SDL_GetTicks();
+  initialize_game(game);
 
   if (!SDL_Init(SDL_INIT_VIDEO)) {
     SDL_Log("Failed to initialize SDL: %s", SDL_GetError());
@@ -65,12 +105,6 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[]) {
   return SDL_APP_CONTINUE;
 }
 
-void RenderBall(float x, float y, float size) {
-  SDL_FRect r = {x - size / 2.0, y - size / 2.0, size, size};
-  SDL_RenderRect(renderer, &r);
-  SDL_RenderFillRect(renderer, &r);
-}
-
 SDL_AppResult SDL_AppIterate(void *appstate) {
   GameState *game = (GameState *)appstate;
   Uint64 now = SDL_GetTicks();
@@ -85,33 +119,66 @@ SDL_AppResult SDL_AppIterate(void *appstate) {
   SDL_RenderRect(renderer, NULL);
   SDL_RenderLine(renderer, WIN_WIDTH / 2.0, 0, WIN_WIDTH / 2.0, WIN_HEIGHT);
 
-  // State
-  float len =
-      SDL_sqrtf(game->ball.dx * game->ball.dx + game->ball.dy * game->ball.dy);
-  if (len > 0) {
-    game->ball.dx /= len;
-    game->ball.dy /= len;
-  }
-  game->ball.x += game->ball.dx * game->ball.speed * delta;
-  game->ball.y += game->ball.dy * game->ball.speed * delta;
+  if (!game->is_paused || game->next_step) {
+    // Update left player
+    if (game->left_player.rect.y <= 5.0) {
+      game->left_player.rect.y = 5.0;
+    } else if (game->left_player.rect.y >=
+               WIN_HEIGHT - game->left_player.rect.h - 5.0) {
+      game->left_player.rect.y = WIN_HEIGHT - game->left_player.rect.h - 5.0;
+    }
+    game->left_player.rect.y +=
+        game->left_player.dy * game->left_player.speed * delta;
 
-  if (game->ball.y <= 0) {
-    game->ball.dy = 1.0;
-  } else if (game->ball.y > WIN_HEIGHT) {
-    game->ball.dy = -1.0;
-  }
+    // Update right player
+    if (game->right_player.rect.y <= 5.0) {
+      game->right_player.rect.y = 5.0;
+    } else if (game->right_player.rect.y >=
+               WIN_HEIGHT - game->right_player.rect.h - 5.0) {
+      game->right_player.rect.y = WIN_HEIGHT - game->right_player.rect.h - 5.0;
+    }
+    game->right_player.rect.y +=
+        game->right_player.dy * game->right_player.speed * delta;
 
-  if (game->ball.x <= 0) {
-    game->ball.dx = 1.0;
-  } else if (game->ball.x > WIN_WIDTH) {
-    game->ball.dx = -1.0;
-  }
+    // Ball state
+    float len = SDL_sqrtf(game->ball.dx * game->ball.dx +
+                          game->ball.dy * game->ball.dy);
+    if (len > 0) {
+      game->ball.dx /= len;
+      game->ball.dy /= len;
+    }
+    game->ball.rect.x += game->ball.dx * game->ball.speed * delta;
+    game->ball.rect.y += game->ball.dy * game->ball.speed * delta;
 
-  SDL_Log("%f,%f | %f,%f", game->ball.x, game->ball.y, game->ball.dx,
-          game->ball.dy);
+    if (game->ball.rect.y <= 0) {
+      game->ball.dy = 1.0;
+    } else if (game->ball.rect.y > WIN_HEIGHT) {
+      game->ball.dy = -1.0;
+    }
+
+    if (SDL_HasRectIntersectionFloat(&game->ball.rect,
+                                     &game->right_player.rect)) {
+      game->ball.dx = -1.0;
+    } else if (SDL_HasRectIntersectionFloat(&game->ball.rect,
+                                            &game->left_player.rect)) {
+      game->ball.dx = 1.0;
+    }
+
+    if (game->ball.rect.x < 0 || game->ball.rect.x > WIN_WIDTH) {
+      initialize_ball(&game->ball);
+    }
+
+    SDL_Log("%f,%f | %f,%f", game->ball.rect.x, game->ball.rect.y,
+            game->ball.dx, game->ball.dy);
+    game->next_step = false;
+  }
 
   // Render
-  RenderBall(game->ball.x, game->ball.y, 20);
+  RenderBall(game->ball.rect.x, game->ball.rect.y, game->ball.rect.w);
+
+  // Render player
+  RenderPlayer(&game->left_player);
+  RenderPlayer(&game->right_player);
 
   SDL_RenderPresent(renderer);
   game->last_ticks = SDL_GetTicks();
@@ -121,11 +188,37 @@ SDL_AppResult SDL_AppIterate(void *appstate) {
 }
 
 SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event) {
+  GameState *game = (GameState *)appstate;
+
   if (event->type == SDL_EVENT_QUIT) {
     return SDL_APP_SUCCESS;
   } else if (event->type == SDL_EVENT_KEY_DOWN) {
     if (event->key.scancode == SDL_SCANCODE_Q) {
       return SDL_APP_SUCCESS;
+    } else if (event->key.scancode == SDL_SCANCODE_R) {
+      initialize_game(game);
+    } else if (event->key.scancode == SDL_SCANCODE_SPACE) {
+      game->is_paused = !game->is_paused;
+    } else if (event->key.scancode == SDL_SCANCODE_PERIOD) {
+      game->next_step = true;
+    } else if (event->key.scancode == SDL_SCANCODE_W) {
+      game->left_player.dy = -1.0;
+    } else if (event->key.scancode == SDL_SCANCODE_S) {
+      game->left_player.dy = 1.0;
+    } else if (event->key.scancode == SDL_SCANCODE_UP) {
+      game->right_player.dy = -1.0;
+    } else if (event->key.scancode == SDL_SCANCODE_DOWN) {
+      game->right_player.dy = 1.0;
+    }
+  } else if (event->type == SDL_EVENT_KEY_UP) {
+    if (event->key.scancode == SDL_SCANCODE_W) {
+      game->left_player.dy = 0.0;
+    } else if (event->key.scancode == SDL_SCANCODE_S) {
+      game->left_player.dy = 0.0;
+    } else if (event->key.scancode == SDL_SCANCODE_UP) {
+      game->right_player.dy = 0.0;
+    } else if (event->key.scancode == SDL_SCANCODE_DOWN) {
+      game->right_player.dy = 0.0;
     }
   }
   return SDL_APP_CONTINUE;
